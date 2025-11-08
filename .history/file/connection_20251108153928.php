c14b80ebb2ed31d9931af415233a6a38ce190dcd
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2025-11-08 17:25:27 +0530

    Stop tracking .history folder

--- .history/file/connection_20251108153928.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "b7qhimpabqtgwt5jr8dk-mysql.services.clever-cloud.com";
-$username = "umexmuin68rw3b3x";
-$password = "";
-$dbname = "bloodbank";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if (!$conn) {
-    die('Could not Connect MySql:' . mysql_error());
-}
